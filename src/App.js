3040fe3b6bab016a8b7d15d86c0ddc868ea9c2aa
Nicola Mössner <nicola.mossner@utopi.co.uk>
2021-03-03 16:04:17 +0000

    Add Glasses and Widget components. Refactor layout to use full screen. Refactor css to styled-components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
+import styled from 'styled-components';
 import './App.css';
-import {Watch} from "./components/Watch";
+import { Glasses } from './components/Glasses';
+import { Video } from './components/Video';
+import { Watch } from "./components/Watch";
 
 function App() {
   return (
     <div className="App">
-      <div className="topSection">
-        <div className="topView"><p>BACKGROUND GOES HERE</p></div>
-      </div>
-      <div className="bottomSection">
-        <Watch/>
-      </div>
+      <Container>
+        <Video />
+        <Glasses />
+        <Watch />
+      </Container>
     </div>
   );
 }
 
 export default App;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+`;
---
+++ src/components/Widgets.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { WidgetTopLeft } from './WidgetTopLeft';
+
+export const Widgets = ({ widthFactor, heightFactor }) => {
+  return (
+    <WidgetTopLeft widthFactor={widthFactor} heightFactor={heightFactor} />
+  )
+}
