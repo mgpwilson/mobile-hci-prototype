f340e49789b979bae5dafa359381be973b82bf1d
Faye McCalpin <rosieandpip@gmail.com>
2021-03-03 16:16:43 +0000

    Key press movement handlers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import {Watch} from "./components/Watch";
+import {Movement} from "./components/MovementHandler";
 
 function App() {
   return (
     <div className="App">
       <div className="topSection">
-        <div className="topView"><p>BACKGROUND GOES HERE</p></div>
+        <div className="topView">
+          <p>BACKGROUND GOES HERE</p>
+          <Movement/>
+        </div>
       </div>
       <div className="bottomSection">
         <Watch/>
