93d38fb6b0666c52719ebfd9e5f9b51f409ba6ca
Faye McCalpin <rosieandpip@gmail.com>
2021-03-13 17:56:59 +0000

    Activity Rings

---
+++ src/components/ActivityRings.js
@@ -0,0 +1,32 @@
+import * as React from "react";
+import Box from "@material-ui/core/Box";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Typography from "@material-ui/core/Typography";
+
+
+export const ActivityRing = (props) => {
+  //const [progress, setProgress] = React.useState(40);
+  return (
+    <Box position="relative" display="inline-flex">
+      <CircularProgress variant="determinate" value={40} color="secondary" {...props} />
+      <Box
+        top={0}
+        left={0}
+        bottom={0}
+        right={0}
+        position="absolute"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        flexDirection='column'
+      >
+        <Typography variant="h5" component="div" color="textPrimary">{`${Math.round(
+          40
+        )}%`}</Typography>
+        <Typography variant="caption" component="div" color="textPrimary">
+          daily goal
+        </Typography>
+      </Box>
+    </Box>
+  )
+}
