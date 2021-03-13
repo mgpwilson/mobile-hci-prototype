5b5cb2adfc046dcd790dab7392963290b4aa6476
Faye McCalpin <rosieandpip@gmail.com>
2021-03-13 18:04:18 +0000

    Time components for date

--- src/components/Time.js
+++ src/components/Time.js
@@ -3,9 +3,15 @@ import Typography from "@material-ui/core/Typography";
 
 
 export const Time = (props) => {
+  function pad(num, size) {
+    num = num.toString();
+    while (num.length < size) num = "0" + num;
+    return num;
+  }
+
   const getTimeNow = () => {
     let currentDate = new Date();
-    let currentTime = currentDate.getHours() + ":" + currentDate.getMinutes() /*+ ":" + currentDate.getSeconds()*/;
+    let currentTime = currentDate.getHours() + ":" + pad(currentDate.getMinutes(), 2) /*+ ":" + currentDate.getSeconds()*/;
     return (currentTime);
   }
 
@@ -20,8 +26,48 @@ export const Time = (props) => {
   }, [time]);
 
   return (
-    <Typography variant={props.variant}>
+    <Typography variant={props.variant} {...props}>
       {time}
+    </Typography>
+  )
+}
+
+export const Weekday = props => {
+  const weekdays = ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"];
+  const now = new Date();
+  const weekday = weekdays[now.getDay()];
+
+  return (
+    <Typography variant={props.variant} {...props}>
+      {weekday}
+    </Typography>
+  )
+}
+
+export const ShortDate = props => {
+  const months = ["Jan", "Feb", "Mar","Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Ded"];
+  let current_datetime = new Date();
+  let formatted_date = months[current_datetime.getMonth()] + " " + current_datetime.getDate();
+  console.log(formatted_date);
+
+  return (
+    <Typography variant={props.variant} {...props}>
+      {formatted_date}
+    </Typography>
+  )
+}
+
+export const LongDate = props => {
+  const now = new Date();
+  const weekdays = ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"];
+  const weekday = weekdays[now.getDay()];
+
+  const months = ["Jan", "Feb", "Mar","Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Ded"];
+  let formatted_date = months[now.getMonth()] + " " + now.getDate();
+
+  return (
+    <Typography variant={props.variant} {...props}>
+      {weekday + " " + formatted_date}
     </Typography>
   )
 }
